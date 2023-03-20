5100c51089d6df1354a481704d19e79e99272c4f
Jennifer Cotte <jennifer.malacain@laposte.net>
2023-03-20 10:20:59 +0100

    ajout des tests pour l'application oc_lettings_site

---
+++ oc_lettings_site/tests/test_view.py
@@ -0,0 +1,11 @@
+from django.urls import reverse
+from django.test import Client
+import pytest
+
+
+@pytest.mark.django_db
+def test_view_index():
+    client = Client()
+    response = client.get(reverse("index"))
+    assert response.status_code == 200
+    assert b'<title>Holiday Homes</title>' in response.content
